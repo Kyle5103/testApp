83810138466b68fe8b24ba46c63b353ab598ee89
Kyle Neely <Kyle.Neely@inin.com>
2016-01-22 21:05:44 -0500

    wip

--- src/main/java/api/Application.java
+++ src/main/java/api/Application.java
@@ -18,25 +18,25 @@ public class Application {
 
     public static void main(String[] args) throws Exception {
         new Application();
-        Jedis jedis = RedisSetup.getConnection();
-        Set<String> keys = jedis.keys("*");
-        for (String key : keys) {
-            jedis.del(key);
-        }
-
-
-
-        for(int i = 0; i < 50; i++){
-            InvoiceResource invoice2 = new InvoiceResource();
-            invoice2.setCompanyName("Comfort Air");
-            invoice2.setInvoiceNo("100" + i);
-            invoice2.setStatus("Incomplete");
-            invoice2.setPrice("41.22");
-            invoice2.setTypeOfWork("Mechanical");
-            invoice2.setComment("Fixed a light bulb.");
-            new InvoiceWorkflow().createInvoice(invoice2);
-        }
-
+//        Jedis jedis = RedisSetup.getConnection();
+//        Set<String> keys = jedis.keys("*");
+//        for (String key : keys) {
+//            jedis.del(key);
+//        }
+//
+//
+//
+//        for(int i = 0; i < 50; i++){
+//            InvoiceResource invoice2 = new InvoiceResource();
+//            invoice2.setCompanyName("Comfort Air");
+//            invoice2.setInvoiceNo("100" + i);
+//            invoice2.setStatus("Incomplete");
+//            invoice2.setPrice("41.22");
+//            invoice2.setTypeOfWork("Mechanical");
+//            invoice2.setComment("Fixed a light bulb.");
+//            new InvoiceWorkflow().createInvoice(invoice2);
+//        }
+//
 
         SpringApplication.run(Application.class, args);
     }
